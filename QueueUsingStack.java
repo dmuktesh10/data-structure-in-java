afca33a2c79b03b50e9cda3280ecadeec825d102
dmuktesh10 <dmuktesh10@gmail.com>
2019-11-10 13:43:09 +0530

    implement queue using stack

--- QueueUsingStack.java
+++ QueueUsingStack.java
@@ -1,9 +1,35 @@
-import mdds.*;
+import java.util.*;
 public class QueueUsingStack
 {
+     Stack<Integer> s1=new Stack<Integer>();
+    Stack<Integer> s2=new Stack<Integer>();
   public static void main(String []args)
   {
-    Stack s1=new Stack();
-    Stack s2=new Stack();
+ 
+    QueueUsingStack q = new QueueUsingStack();
+    q.enQueue(1);  
+    q.enQueue(2);  
+    q.enQueue(3);  
+  
+    System.out.println(q.deQueue());  
+    System.out.println(q.deQueue()); 
+    System.out.println(q.deQueue()); 
+  }
+
+  void enQueue(int val)
+  {
+    s1.push(val);
+  }
+  int deQueue()
+  {
+    if(s2.isEmpty())
+    {
+      while(!s1.isEmpty())
+      {
+        int p=s1.pop();
+        s2.push(p);
+      }
+    }
+    return s2.pop();
   }
 }
